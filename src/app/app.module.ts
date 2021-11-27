9c076fd49549726aefb78b91bff747af97f59ac9
Nahid Delic <nahid.delic@fet.ba>
2021-11-27 14:35:29 +0100

    removed unused modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { TuiRootModule, TuiDialogModule, TuiNotificationsModule, TUI_SANITIZER, TuiButtonModule, TuiSvgModule } from "@taiga-ui/core";
+import { TuiRootModule, TuiButtonModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {TuiIslandModule} from '@taiga-ui/kit';
@@ -8,9 +8,6 @@ import {TuiIslandModule} from '@taiga-ui/kit';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import {TuiGroupModule} from '@taiga-ui/core';
-
-import { TuiAxesModule, TuiLineChartModule, TuiLineDaysChartModule } from "@taiga-ui/addon-charts";
 import { KucaComponent } from './components/kuca/kuca.component';
 import { ControlsComponent } from './components/controls/controls.component';
 import { NgxGaugeModule } from "ngx-gauge";
@@ -29,15 +26,8 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
     AppRoutingModule,
       TuiRootModule,
       BrowserAnimationsModule,
-      TuiDialogModule,
-      TuiNotificationsModule,
       TuiIslandModule,
-      TuiGroupModule,
       TuiButtonModule,
-      TuiSvgModule,
-      TuiAxesModule,
-      TuiLineChartModule,
-      TuiLineDaysChartModule,
       NgxGaugeModule
 ],
   providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
