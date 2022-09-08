dfd95a96ab0fea016a8c4848a35dc81f2b8b5e9f
Nahid Delic <ndelic@capeannenterprises.com>
2022-09-08 10:47:51 +0200

    removed password

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,27 +6,8 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  isActivated: boolean = false;
-  form:FormGroup = new FormGroup({
-    valid: new FormControl('')
-  }) 
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-    if(localStorage.getItem('isActivated') === 'true') {
-      this.isActivated = true;
-    }
-  }
-
-  provjeri() {
-    if(this.form.controls['valid'].value === "jakpasswordbgm") {
-      this.isActivated=true;
-      localStorage.setItem('isActivated','true');
-      return;
-    } else {
-      return;
-    }
-  }
+    ngOnInit(): void { }
 
 }
